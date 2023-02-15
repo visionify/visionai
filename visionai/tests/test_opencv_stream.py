1625d4f104651da21111feb97735663c4cbe6147
Harsh Murari <hmurari@visionify.ai>
2023-02-15 10:49:37 -0700

    Fix test error bugs
    Former-commit-id: 9e4cff19576575ff1f4c73243a49b9dc6ab73a50
    Former-commit-id: c57b21cd5cc5d825bee06677f2a677aa6c313237

--- visionai/tests/test_opencv_stream.py
+++ visionai/tests/test_opencv_stream.py
@@ -1,24 +1,28 @@
 import cv2
-from imutils.video import WebcamVideoStream
 import time
 
-rtsp_url = "rtsp://username:password@ip_address:port_number/stream_name"
-fourcc = cv2.VideoWriter_fourcc(*"H264")
+def opencv_stream():
 
-src = 0 # 0 for the default camera
-vs = WebcamVideoStream(src).start()
-time.sleep(2.0) # Wait for the camera to warm up
+    from imutils.video import WebcamVideoStream
+    rtsp_url = "rtsp://username:password@ip_address:port_number/stream_name"
+    fourcc = cv2.VideoWriter_fourcc(*"H264")
 
-out = cv2.VideoWriter(rtsp_url, fourcc, 10, (640, 480), True)
+    src = 0 # 0 for the default camera
+    vs = WebcamVideoStream(src).start()
+    time.sleep(2.0) # Wait for the camera to warm up
 
-while True:
-    frame = vs.read()
-    out.write(frame)
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord("q"):
-        break
+    out = cv2.VideoWriter(rtsp_url, fourcc, 10, (640, 480), True)
 
-out.release()
-vs.stop()
-cv2.destroyAllWindows()
+    while True:
+        frame = vs.read()
+        out.write(frame)
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
+
+    out.release()
+    vs.stop()
+    cv2.destroyAllWindows()
+
+
