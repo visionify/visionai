1c3f00dc34b6e4e26301cdfff2afad6fb5b6a66c
akil-visionify <akil@visionify.ai>
2023-02-17 04:17:01 +0530

    smoke and fire detection code is added with event firing
    Former-commit-id: 0ecd054a40abc33d62c97e34b8976b4926ff3c19
    Former-commit-id: 19d64a6bfec62473af31c20451098c59ab548815

--- visionai/scenarios/smoke_and_fire_detection.py
+++ visionai/scenarios/smoke_and_fire_detection.py
@@ -12,11 +12,22 @@ from util.general import LOGGER
 from scenarios import Scenario
 from config import TRITON_HTTP_URL
 
+from events.events_engine import EventsEngine
+from enum import Enum
+
+class Event(str, Enum):
+    DEBUG = 'DEBUG'
+    INFO = 'INFO'
+    WARNING = 'WARNING'
+    ERROR = 'ERROR'
+    CRITICAL = 'CRITICAL'
+
 class SmokeAndFireDetection(Scenario):
     def __init__(self, scenario_name, camera_name=0, events=None, triton_url=TRITON_HTTP_URL):
 
         from models.triton_client_yolov5 import yolov5_triton
         self.model = yolov5_triton(triton_url, scenario_name)
+        self.f_event =  EventsEngine(use_redis=True)
         super().__init__(scenario_name, camera_name, events, triton_url)
 
 
@@ -32,7 +43,7 @@ class SmokeAndFireDetection(Scenario):
 
         print(f'Opening capture for {stream}')
         video = cv2.VideoCapture(stream)
-
+        fire_count = 0
         while True:
             # Do processing
             ret, frame = video.read()
@@ -43,6 +54,18 @@ class SmokeAndFireDetection(Scenario):
 
             # Detect smoke & fire
             results = self.model(frame, size=640)  # batched inference
+
+            det = results.xyxy[0]
+
+            if len(det):
+                for *xyxy, conf, cls in reversed(det):
+                    if int(cls.item()) == 0:
+                        fire_count += 1
+
+            if fire_count > 5:
+                self.f_event.fire_event(Event.CRITICAL, 'WEBCAM', "smoke-and-fire-detection", 'FIRE_DETECTED', {})
+
+
             results.print()
             results.show()
             # if results contains smoke or fire class --> then fire an event here.
@@ -52,3 +75,9 @@ class SmokeAndFireDetection(Scenario):
             if self.stop_evt.is_set():
                 break
 
+def camera_stream():
+    snf = SmokeAndFireDetection(scenario_name = 'smoke-and-fire-detection')
+    snf.start(camera_name=0)
+
+if __name__ == '__main__':
+    camera_stream()
